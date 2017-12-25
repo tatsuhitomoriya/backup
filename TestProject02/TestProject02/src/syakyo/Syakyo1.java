13170b504ed1a8d2019807dc569e7f5db063fe09
tatsuhitomoriya <34647114+tatsuhitomoriya@users.noreply.github.com>
2017-12-25 16:09:50 +0900

    Add files via upload

---
+++ TestProject02/TestProject02/src/syakyo/Syakyo1.java
@@ -0,0 +1,12 @@
+package syakyo;
+
+public class Syakyo1 {
+
+	public static void main(String[] args) {
+		// ここにコードを挿入
+		String abc = "山田";
+		System.out.println(abc + "さん");
+
+	}
+
+}
---
+++ TestProject02/src/syakyo/Syakyo1.java
@@ -0,0 +1,12 @@
+package syakyo;
+
+public class Syakyo1 {
+
+	public static void main(String[] args) {
+		// ここにコードを挿入
+		String[] name = { "田中", "高橋", "斎藤" };
+		name[0] = "加藤";
+		System.out.println(name[0]);
+
+	}
+}
