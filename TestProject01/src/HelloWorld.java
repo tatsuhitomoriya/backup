669f832ccd87675aad92bb5a52b8cf705174e778
hirakata09231025 <tinolwm.f@docomo.ne.jp>
2017-12-25 21:46:11 +0900

    12/25

--- TestProject01/src/HelloWorld.java
+++ TestProject01/src/HelloWorld.java
@@ -3,8 +3,12 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// Hello World! の出力
-		String tel="090-1234-5678";
-		System.out.println(tel);
+		int i=10;
+		do{
+			System.out.println(i);
+			i-=3;
+		}while(i>=1);
 	}
+
 
 }
