30666bda6fcfe818e24664b4f3f80d534da21f40
tatsuhitomoriya <tmj4oc3s_q26@i.softbank.jp>
2018-01-05 21:51:09 +0900

    1/5

---
+++ TestDB2/src/Test.java
@@ -0,0 +1,12 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		TestUserDAO dao=new TestUserDAO();
+		dao.select("taro", "123");
+		dao.selectAll();
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
